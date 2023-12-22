93aaa0190518dad27ad593c3762183505e736593
non-fungible-user <oleh.pashchenko+1@gmail.com>
2023-12-22 12:39:35 +0100

    added survey chart

--- be/src/server.ts
+++ be/src/server.ts
@@ -7,8 +7,8 @@ import app from './app';
 
 
 const server = http.createServer(app);
-console.log()
 const port = process.env.BE_PORT || 3000;
+
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
@@ -22,4 +22,5 @@ process.on('uncaughtException', (error) => {
 
   process.exit(1);
 });
+
 export default server;
